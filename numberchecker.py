e546a530fcbd7cbdfb60b1beba3f7581f3add2ec
Packan <ariaxero@gmail.com>
2022-09-07 13:44:10 +0200

    Create numberchecker.py

---
+++ numberchecker.py
@@ -0,0 +1,12 @@
+state = 1
+
+while state == 1:
+    num = int(input("Skirv en siffra: "))
+
+    if num > 0:
+        print("positivt")
+
+    elif num < 0:
+        print ("negativt")
+
+    state = int(input("Fortsätta? 1. Ja, 2. Nej: "))
