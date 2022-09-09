3b85849fbc42a86b4790931b445db65e4585533b
Packan <ariaxero@gmail.com>
2022-09-09 14:44:10 +0200

    Update class.py

--- class.py
+++ class.py
@@ -1,13 +1,24 @@
+# Defining car class
 class Car:
     def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
+        self._brand = brand
+        self._model = model
+        self._year = year
 
     def __str__(self):
-        return str(self.brand) + "\n" + str(self.model) + "\n" + (self.year)
+        return "{} {} {}".format(self._brand, self._model, self._year)
+# Making the car object
+def make_car():
+    user_car = {}
+    state = int(input("Would you like to make a car?\n\n1. Yes\n2. No\n\n"))
 
-car1 = str(Car("Mercedes", "E53 Coupé", "2022"))
-car2 = str(Car("BMW", "M8 Grand Coupé", "2020"))
+    if state == 1:
+        print("Making a new car!\n")
+        user_car["brand"] = input("Which brand is it?: ")
+        user_car["model"] = input("Which model is it?: ")
+        user_car["year"] = input("Which year is it?: ")
+        return user_car
 
-print(car1, car2, car2, car1, sep="\n\n")
+# Printing the car
+car = Car(**make_car())
+print(car)
